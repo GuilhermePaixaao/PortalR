62c3fa6003c68f6d168ce460fc5f3cfdb48b4f7c
GuilhermePaixaao <guilhermefreitaspaixao81@gmail.com>
2025-10-31 14:07:26 -0300

    Iniciando.

--- src/routers/chatRouter.js
+++ src/routers/chatRouter.js
@@ -7,6 +7,5 @@ const router = express.Router();
 // Define a rota POST para /
 // (Ela vai virar /api/chat no seu roteador principal, o index.js)
 router.post('/', handleChat);
-
 // Usa 'export default' para exportar o roteador
 export default router;
