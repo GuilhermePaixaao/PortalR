7160b1defb46e3bc30a80a4143be0cad41127dc8
GuilhermePaixaao <guilhermefreitaspaixao81@gmail.com>
2025-11-05 15:33:50 -0300

    .

--- src/routers/categoriaRouter.js
+++ src/routers/categoriaRouter.js
@@ -6,5 +6,5 @@ const router = Router();
 router.get('/categorias', CategoriaController.listarCategorias);
 router.post('/categorias', CategoriaController.criarCategoria); 
 router.put('/categorias/:id', CategoriaController.atualizarCategoria); 
-router.delete('/categorias/:id', CategoriaController.deletarCategoria); 
+router.delete('/categorias/:id', CategoriaController.excluirCategoria); 
 export default router;
