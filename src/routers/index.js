878bb145b874ec2f998e18a033ca82fdf3e623e5
GuilhermePaixaao <guilhermefreitaspaixao81@gmail.com>
2025-11-05 16:37:55 -0300

    .

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,29 +2,19 @@ import { Router } from 'express';
 
 // 1. Importe os routers que você já tem
 import categoriaRouter from './categoriaRouter.js';
-// (Presumindo que você tenha outros, ex: funcionarioRouter, etc.)
-// import funcionarioRouter from './funcionarioRouter.js'; 
-// import chamadoRouter from './chamadoRouter.js';
-// ... e outros que você usa ...
-
-
-// 2. ADICIONE A IMPORTAÇÃO DO NOVO ROUTER DE SUBCATEGORIA
-// (Use o nome de arquivo exato que você criou, ex: subCategoriaRouter.js)
-import subCategoriaRouter from './subCategoriaRouter.js';
-
+import funcionarioRouter from './funcionarioRouter.js'; 
+import chamadoRouter from './chamadoRouter.js';
+import subCategoriaRouter from './subcategoriaRouter.js';
 
 const router = Router();
 
-// 3. Use os routers que você já tem
+// 3. Use os routers
 router.use(categoriaRouter);
-// router.use(funcionarioRouter);
-// router.use(chamadoRouter);
+router.use(funcionarioRouter);
+router.use(chamadoRouter);
 // ...
 
-
-// 4. ADICIONE O USO DO NOVO ROUTER
-// É aqui que você "ativa" o router de subcategorias
+// 4. Use o router corrigido
 router.use(subCategoriaRouter);
-
 
 export default router;
