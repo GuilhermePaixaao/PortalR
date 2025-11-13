16d00db81b60d8aec2a75b2518c54d5fadd00431
GuilhermePaixaao <guilhermefreitaspaixao81@gmail.com>
2025-11-13 14:29:17 -0300

    .

--- src/routers/index.js
+++ src/routers/index.js
@@ -6,7 +6,6 @@ import categoriaRouter from './categoriaRouter.js';
 import chamadoRouter from './chamadoRouter.js';
 import chatRouter from './chatRouter.js';
 import funcionarioRouter from './funcionarioRouter.js';
-import whatsappRouter from './whatsappRouter.js';
 
 // (REMOVIDO) O router de subcategoria não é mais necessário
 // import subcategoriaRouter from './sbcategoriaRouter.js';
@@ -19,7 +18,6 @@ router.use(categoriaRouter);
 router.use(chamadoRouter);
 router.use(chatRouter);
 router.use(funcionarioRouter);
-router.use(whatsappRouter);
 // (REMOVIDO) O router de subcategoria não é mais necessário
 // router.use(subcategoriaRouter); 
 
