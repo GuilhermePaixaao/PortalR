a6c3e95eec75ed45579ddfe1699213f687b86e30
GuilhermePaixaao <guilhermefreitaspaixao81@gmail.com>
2025-11-17 13:06:51 -0300

    .

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "https://portal.smrosalina.com.br", // <-- CORRIGIDO
+    origin: "*", // <-- ATUALIZADO (antes era "https://portal.smrosalina.com.br")
     methods: ["GET", "POST"]
   }
 });
