a8a3add9e501cccb4c264a75ff0f119258ab82a3
GuilhermePaixaao <guilhermefreitaspaixao81@gmail.com>
2025-12-02 10:11:49 -0300

    .

--- src/routers/categoriaRouter.js
+++ src/routers/categoriaRouter.js
@@ -3,8 +3,10 @@ import * as CategoriaController from '../controllers/categoriaController.js';
 
 const router = Router();
 
-router.get('/categorias', CategoriaController.listarCategorias);
-router.post('/categorias', CategoriaController.criarCategoria); 
-router.put('/categorias/:id', CategoriaController.atualizarCategoria); 
-router.delete('/categorias/:id', CategoriaController.excluirCategoria); 
+// Adicionado o prefixo '/api' para alinhar com o frontend
+router.get('/api/categorias', CategoriaController.listarCategorias);
+router.post('/api/categorias', CategoriaController.criarCategoria); 
+router.put('/api/categorias/:id', CategoriaController.atualizarCategoria); 
+router.delete('/api/categorias/:id', CategoriaController.excluirCategoria); 
+
 export default router;
